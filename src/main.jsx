5b3874f7ce648700c583f4b315995f96d8a1e269
Jairo Matías <jairo_1829@hotmail.com>
2026-09-04 15:53:44 -0400

    feat: wire app router for mockup UI

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
-import "./styles/reset.css";
-import "./styles/variables.css";
-import "./styles/global.css";
-import "./styles/utilities.css";
-import "./styles/animations.css";
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
